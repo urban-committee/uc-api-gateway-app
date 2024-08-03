657dc41e2746ee475f21d2e9f7ee51f7b2e60091
Pdubey1994 <committeeurban@gmail.com>
2024-08-03 19:49:02 +0530

    Initial commit

---
+++ src/main/java/com/urban/committee/UcApiGatewayAppApplication.java
@@ -0,0 +1,13 @@
+package com.urban.committee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcApiGatewayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcApiGatewayAppApplication.class, args);
+	}
+
+}
